12e2a8c9714bd3f56253c27b15be83896227b5e0
Rhonda Glennon <rhondaglennon@gmail.com>
2018-10-22 15:30:44 -0400

    add pop-ups to map

--- map.js
+++ map.js
@@ -1,7 +1,18 @@
-// $(document).ready(function(){
+var map = L.map("map").setView([37.75, -122.23], 10);
 
-    var map = L.map("map").setView([37.75, -122.23], 10);
+L.esri.basemapLayer("Topographic").addTo(map);
 
-    L.esri.basemapLayer("Topographic").addTo(map);
+var marker = L.marker([37.7814318,-122.4030528,]).addTo(map);
 
-// });
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+
+map.on('click', onMapClick);
