80829d657781d306f8b8f8bc50a20b7d118cd176
Rhonda Glennon <rhondaglennon@gmail.com>
2018-10-23 14:46:00 -0400

    add geojson via ajax call

--- map.js
+++ map.js
@@ -1,19 +1,7 @@
-var map = L.map("map").setView([39.74739, -105], 13);
+var map = L.map("map")
+    .setView([37.7877, -122.4232], 14);
+var basemap = L.esri.basemapLayer("Topographic").addTo(map);
 
-L.esri.basemapLayer("Topographic").addTo(map);
-
-var coorsField = {
-    "type": "Feature",
-    "properties": {
-        "popupContent": "Hello"
-    },
-    "geometry": {
-        "type": "Point",
-        "coordinates": [-104.99404191970824, 39.756213909328125]
-    }
-};
-
-//marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
 
 var geojsonMarkerOptions = {
     radius: 12,
@@ -30,11 +18,43 @@ function onEachFeature(feature, layer) {
     if (feature.properties && feature.properties.popupContent) {
         layer.bindPopup(feature.properties.popupContent);
     }
+    // captures click on existing feature
+    layer.on("click", function (e) {
+        console.log("meow")
+    });
 }
 
-L.geoJSON(coorsField, {
-    pointToLayer: function (feature, latlng) {
-        return L.circleMarker(latlng, geojsonMarkerOptions);
-    },
-    onEachFeature: onEachFeature
-}).addTo(map);
+var popup = L.popup();
+
+var marker = L.marker();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+    marker
+        .setLatLng(e.latlng)
+        .addTo(map);
+}
+
+map.on('click', onMapClick);
+
+var points = $.ajax({
+    url: "point-map.geojson",
+    dataType: "json",
+    success: console.log("geojson data successfully loaded."),
+    error: function (xhr) {
+        alert(xhr.statusText)
+    }
+})
+// Specify that this code should run once the data request is complete
+$.when(points).done(function () {
+    //L.geoJSON(points.responseJSON).addTo(map);
+    let myPoint = L.geoJSON(points.responseJSON, {
+        pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng, geojsonMarkerOptions);
+        },
+        onEachFeature: onEachFeature
+    }).addTo(map);
+});
